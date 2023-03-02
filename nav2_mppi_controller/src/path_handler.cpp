8ad8942d8f70d02f125bff19755a7d135d637e66
Guillaume Doisy <guillaume@dexory.com>
2023-03-02 18:12:17 +0000

    remove merge artefact

--- nav2_mppi_controller/src/path_handler.cpp
+++ nav2_mppi_controller/src/path_handler.cpp
@@ -48,7 +48,7 @@ PathHandler::getGlobalPlanConsideringBoundsInCostmapFrame(
   // Don't search further away than the prune distance
   auto closest_pose_upper_bound =
     nav2_util::geometry_utils::first_after_integrated_distance(
-    global_plan_.poses.begin(), global_plan_end_,
+    global_plan_.poses.begin(), global_plan_.poses.end(),
         std::min(max_robot_pose_search_dist_, prune_distance_));
 
   // Find closest point to the robot
