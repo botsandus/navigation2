89af53b9b0b15e2665904026d9f8dd98cbb35d6a
Guillaume Doisy <guillaume@dexory.com>
2024-02-10 16:59:22 +0000

    [DEX] keep nav2 copy_all_parameters instead of rolling  rclcpp version

--- nav2_util/include/nav2_util/node_utils.hpp
+++ nav2_util/include/nav2_util/node_utils.hpp
@@ -91,11 +91,11 @@ std::string time_to_string(size_t len);
  */
 template<typename NodeT>
 void declare_parameter_if_not_declared(
-  NodeT node,
-  const std::string & param_name,
-  const rclcpp::ParameterValue & default_value,
-  const rcl_interfaces::msg::ParameterDescriptor & parameter_descriptor =
-  rcl_interfaces::msg::ParameterDescriptor())
+    NodeT node,
+    const std::string & param_name,
+    const rclcpp::ParameterValue & default_value,
+    const rcl_interfaces::msg::ParameterDescriptor & parameter_descriptor =
+    rcl_interfaces::msg::ParameterDescriptor())
 {
   if (!node->has_parameter(param_name)) {
     node->declare_parameter(param_name, default_value, parameter_descriptor);
@@ -112,11 +112,11 @@ void declare_parameter_if_not_declared(
  */
 template<typename NodeT>
 void declare_parameter_if_not_declared(
-  NodeT node,
-  const std::string & param_name,
-  const rclcpp::ParameterType & param_type,
-  const rcl_interfaces::msg::ParameterDescriptor & parameter_descriptor =
-  rcl_interfaces::msg::ParameterDescriptor())
+    NodeT node,
+    const std::string & param_name,
+    const rclcpp::ParameterType & param_type,
+    const rcl_interfaces::msg::ParameterDescriptor & parameter_descriptor =
+    rcl_interfaces::msg::ParameterDescriptor())
 {
   if (!node->has_parameter(param_name)) {
     node->declare_parameter(param_name, param_type, parameter_descriptor);
@@ -134,23 +134,42 @@ void declare_parameter_if_not_declared(
  */
 template<typename NodeT>
 std::string get_plugin_type_param(
-  NodeT node,
-  const std::string & plugin_name)
+    NodeT node,
+    const std::string & plugin_name)
 {
   declare_parameter_if_not_declared(node, plugin_name + ".plugin", rclcpp::PARAMETER_STRING);
   std::string plugin_type;
   try {
     if (!node->get_parameter(plugin_name + ".plugin", plugin_type)) {
       RCLCPP_FATAL(
-        node->get_logger(), "Can not get 'plugin' param value for %s", plugin_name.c_str());
+          node->get_logger(), "Can not get 'plugin' param value for %s", plugin_name.c_str());
       throw std::runtime_error("No 'plugin' param for param ns!");
     }
   } catch (rclcpp::exceptions::ParameterUninitializedException & ex) {
     RCLCPP_FATAL(node->get_logger(), "'plugin' param not defined for %s", plugin_name.c_str());
     throw std::runtime_error("No 'plugin' param for param ns!");
   }
-
+  
   return plugin_type;
+}
+
+/**
+ * @brief A method to copy all parameters from one node (parent) to another (child).
+ * May throw parameter exceptions in error conditions
+ * @param parent Node to copy parameters from
+ * @param child Node to copy parameters to
+ */
+template<typename NodeT1, typename NodeT2>
+void copy_all_parameters(const NodeT1 & parent, const NodeT2 & child)
+{
+  using Parameters = std::vector<rclcpp::Parameter>;
+  std::vector<std::string> param_names = parent->list_parameters({}, 0).names;
+  Parameters params = parent->get_parameters(param_names);
+  for (Parameters::const_iterator iter = params.begin(); iter != params.end(); ++iter) {
+    if (!child->has_parameter(iter->get_name())) {
+      child->declare_parameter(iter->get_name(), iter->get_parameter_value());
+    }
+  }
 }
 
 /**
