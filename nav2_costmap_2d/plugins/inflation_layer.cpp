d217140bb9395836a0880aede23a44d64b66963f
Tony Najjar <tony.najjar@dexory.com>
2026-02-01 21:05:00 +0000

    Increase cost_lut_precision default to 100 for better accuracy

--- nav2_costmap_2d/plugins/inflation_layer.cpp
+++ nav2_costmap_2d/plugins/inflation_layer.cpp
@@ -68,7 +68,7 @@ InflationLayer::InflationLayer()
   inflate_unknown_(false),
   inflate_around_unknown_(false),
   cell_inflation_radius_(0),
-  cost_lut_precision_(10),
+  cost_lut_precision_(100),
   resolution_(0),
   last_min_x_(std::numeric_limits<double>::lowest()),
   last_min_y_(std::numeric_limits<double>::lowest()),
@@ -96,7 +96,7 @@ InflationLayer::onInitialize()
   declareParameter("cost_scaling_factor", rclcpp::ParameterValue(10.0));
   declareParameter("inflate_unknown", rclcpp::ParameterValue(false));
   declareParameter("inflate_around_unknown", rclcpp::ParameterValue(false));
-  declareParameter("cost_lut_precision", rclcpp::ParameterValue(10));
+  declareParameter("cost_lut_precision", rclcpp::ParameterValue(100));
 
   {
     auto node = node_.lock();
