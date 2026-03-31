acd950bd13abd075dbcd5c79e041d53f76abb068
Tony Najjar <tony.najjar@dexory.com>
2026-03-31 13:44:45 +0000

    match timeouts
    Signed-off-by: Tony Najjar <tony.najjar@dexory.com>

--- nav2_scenario_tester/nav2_scenario_tester/test_runner.py
+++ nav2_scenario_tester/nav2_scenario_tester/test_runner.py
@@ -116,7 +116,7 @@ class NavTestRunner(Node):
         self,
         initial_pose: Pose,
         goal_pose: Pose,
-        timeout: float = 20.0,
+        timeout: float = 60.0,
         settle_time: float = 1.0,
         limits: dict = None,
         fail_fast: bool = False,
