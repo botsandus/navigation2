5df9fc09ea95d19fe24415ae9d69a4e8e7a22326
Tony Najjar <tony.najjar@dexory.com>
2026-04-01 15:34:00 +0000

    move nav2_scenario_tester to own repo
    Signed-off-by: Tony Najjar <tony.najjar@dexory.com>

--- nav2_scenario_tester/nav2_scenario_tester/metrics/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Metrics collectors for nav2_scenario_tester."""
-
-from nav2_scenario_tester.metrics._base import MetricsCollector  # noqa: F401
-from nav2_scenario_tester.metrics.costmap import CostmapMetrics  # noqa: F401
-from nav2_scenario_tester.metrics.odometry import OdometryMetrics  # noqa: F401
-from nav2_scenario_tester.metrics.plan import PlanMetrics  # noqa: F401
-from nav2_scenario_tester.metrics.tracking import TrackingMetrics  # noqa: F401
